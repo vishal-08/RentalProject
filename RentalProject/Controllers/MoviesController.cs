7d25c7ed88298ddbd1d8f4596bb4eca5da7e17f8
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-17 13:30:14 +0530

    files_added-first commit

---
+++ RentalProject/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using RentalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Deadpool !!" };
+            return View(movie);
+        }
+    }
+}
