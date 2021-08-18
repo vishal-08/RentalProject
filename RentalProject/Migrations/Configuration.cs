11e4050d81819cc0f1ce847e084b059c612edf8d
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-18 13:47:41 +0530

    initial migration created

---
+++ RentalProject/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace RentalProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RentalProject.Models.DBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RentalProject.Models.DBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ RentalProject/Models/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace RentalProject.Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("dbconnection")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; } // My domain models
+        public DbSet<Movie> Movies { get; set; }// My domain models
+    }
+}
