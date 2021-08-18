a3cd09ed797b60bc9fbf039bdfcd8825937ad7d6
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-18 14:17:14 +0530

    annotation applied to cuatomer

---
+++ RentalProject/Migrations/202108180846147_ApplyAnnotationtoCuatomer.cs
@@ -0,0 +1,18 @@
+﻿namespace RentalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAnnotationtoCuatomer : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
--- RentalProject/Models/Customer.cs
+++ RentalProject/Models/Customer.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace RentalProject.Models
 {
     public class Customer
-    {
-        public int Id { get; set; }
+    {   
 
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public String Name { get; set; }
 
         public bool IsSubscribedToNewsLetter { get; set; }
