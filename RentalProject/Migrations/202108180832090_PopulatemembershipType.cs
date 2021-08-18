d4c87d067fc6a8c874297a8260bb189f7d651f14
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-18 14:10:20 +0530

    membership table populated using migration

---
+++ RentalProject/Migrations/202108180832090_PopulatemembershipType.cs
@@ -0,0 +1,20 @@
+﻿namespace RentalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulatemembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Insert into MembershipTypes (Id, SignUpFee, DurationInMonth, DiscountRate) Values (1, 0, 0, 0)");
+            Sql("Insert into MembershipTypes (Id, SignUpFee, DurationInMonth, DiscountRate) Values (2, 30, 2, 40)");
+            Sql("Insert into MembershipTypes (Id, SignUpFee, DurationInMonth, DiscountRate) Values (3, 45, 1, 10)");
+            Sql("Insert into MembershipTypes (Id, SignUpFee, DurationInMonth, DiscountRate) Values (4, 23, 2, 22)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
