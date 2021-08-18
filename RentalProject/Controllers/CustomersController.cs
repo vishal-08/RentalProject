23a0df31d984d7957987e319570f8d326bae5778
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-18 14:26:54 +0530

    data fetched from database

--- RentalProject/Controllers/CustomersController.cs
+++ RentalProject/Controllers/CustomersController.cs
@@ -9,30 +9,39 @@ namespace RentalProject.Controllers
 {
     public class CustomersController : Controller
     {
+        private DBContext _context;
+        public CustomersController()
+        {
+            _context = new DBContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customer = GetCustomers();
+            var customer = _context.Customers.ToList();
 
             return View(customer);
         }
         public ActionResult Detail(int Id)
         {
            
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
             if (customer == null)
             {
                 return HttpNotFound();
             }
             return View(customer);
         }
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1,Name = "Ramji Kasuddin"},
-                new Customer {Id = 2,Name = "Yuvraj"}
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Id = 1,Name = "Ramji Kasuddin"},
+        //        new Customer {Id = 2,Name = "Yuvraj"}
+        //    };
+        //}
     }
 }
