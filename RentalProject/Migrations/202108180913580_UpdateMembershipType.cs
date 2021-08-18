690b48882644d14bf080f3dcdb32ac361039dc24
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-18 14:49:30 +0530

    membershiptype updated

---
+++ RentalProject/Migrations/202108180913580_UpdateMembershipType.cs
@@ -0,0 +1,19 @@
+﻿namespace RentalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET Discountrate = 10  WHERE Id = 2; ");
+            Sql("UPDATE MembershipTypes SET SignUpFee = 90, DurationInMonth = 6 , Discountrate = 15  WHERE Id = 3; ");
+            Sql("UPDATE MembershipTypes SET SignUpFee = 120, DurationInMonth = 12 , Discountrate = 15  WHERE Id = 4; ");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
