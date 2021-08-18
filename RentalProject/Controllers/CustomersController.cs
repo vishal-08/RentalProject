fa4dc87551dd7b68e2a51dde540887f897430577
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-18 12:14:30 +0530

    exercise_1 customer detail showed

---
+++ RentalProject/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using RentalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalProject.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customer = GetCustomers();
+
+            return View(customer);
+        }
+        public ActionResult Detail(int Id)
+        {
+           
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1,Name = "Ramji Kasuddin"},
+                new Customer {Id = 2,Name = "Yuvraj"}
+            };
+        }
+    }
+}
