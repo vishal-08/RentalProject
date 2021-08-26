8f04d8e3d3997228cdfd71141b51615defd56fc0
Vishal Chaudhary <vishuchoudhary0102@gmail.com>
2021-08-26 16:17:31 +0530

    movies model changed

---
+++ RentalProject/Migrations/202108261044144_MoviesValidationAdded.cs
@@ -0,0 +1,18 @@
+﻿namespace RentalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MoviesValidationAdded : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Name", c => c.String());
+        }
+    }
+}
--- RentalProject/Models/Movie.cs
+++ RentalProject/Models/Movie.cs
@@ -8,15 +8,20 @@ namespace RentalProject.Models
 {
     public class Movie
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public String Name { get; set; }
 
         public Genre Genre{ get; set; }
         [Display (Name = "Genre")]
+        [Required]
         public int GenreId { get; set; }
         [Display (Name ="Release Date")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
         public DateTime AddedDate { get; set; }
+        [Required]
         [Display (Name = "Stock Available")]
         public Byte NumberInStock { get; set; }
     }
